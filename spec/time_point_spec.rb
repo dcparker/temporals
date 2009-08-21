84393f4befd6d70bc0c0b47411a87b6fa2e0274c
Daniel Parker <dcparker@gmail.com>
2009-08-21 14:02:34 -0400

    Moved into folders for gem

--- spec/time_point_spec.rb
+++ spec/time_point_spec.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'spec'
-require File.dirname(__FILE__) + '/time_point'
+require File.dirname(__FILE__) + '../lib/time_point'
 
 describe TimePoint do
   it "1st-2nd And 4th Thursdays Of March And April 5-6:30pm And March 16th - 24th At 2-2:30pm" do
